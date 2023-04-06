e2ab0689fc3d19bba7f691b9d15f51d979ac7cfe
Eric Martindale <eric@ericmartindale.com>
2023-04-06 08:14:58 -0400

    Fix bug in luck

--- functions/maybeEncounter.js
+++ functions/maybeEncounter.js
@@ -3,15 +3,19 @@ module.exports = function (CHANCE_TABLE = {
   types: ['battle']
 }) {
   return new Promise((resolve, reject) => {
+    // How lucky are we?
     const luck = Math.random();
     const sum = CHANCE_TABLE.probabilities.reduce((a, b) => a + b);
-    console.log('sum:', sum);
+
+    // Luck out
     if (luck > sum) resolve(null);
 
+    // Select encounter type
     const encounter = {
-      type: CHANCE_TABLE.types[Math.floor(Math.random() * CHANCE_TABLE.types.length)]
+      type: CHANCE_TABLE.types[Math.floor(luck * CHANCE_TABLE.types.length)]
     };
 
+    // Serve the encounter
     resolve(encounter);
   });
 };
